119497617e62c3b05d0e55a5007e91ee7d108a44
Tanya <duka.tanik@gmail.com>
2022-04-11 17:56:56 +0300

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,8 @@
 const BASE_URL = 'https://restcountries.com/v3.1/name';
 const params = 'fields=name,capital,population,flags,languages'
 
+
+
 export default function fetchCountries(country) {
     return fetch(`${BASE_URL}/${country}?${params}`)
         .then(response => {
--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import fetchCountries from './fetchCountries.js'
 
 const DEBOUNCE_DELAY = 300;
 
+
+
 const textInput = document.querySelector('input#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
