0582714809064ce872ca4624a708e401750e7bf2
Tanya <duka.tanik@gmail.com>
2022-04-11 21:15:46 +0300

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,9 +1,10 @@
-const BASE_URL = 'https://restcountries.com/v3.1/name';
-const params = 'fields=name,capital,population,flags,languages'
+
 
 
 
 export default function fetchCountries(country) {
+    const BASE_URL = 'https://restcountries.com/v3.1/name';
+    const params = 'fields=name,capital,population,flags,languages'
     console.log(`${BASE_URL}/${country}?${params}`);
     return fetch(`${BASE_URL}/${country}?${params}`)
         .then(response => {
