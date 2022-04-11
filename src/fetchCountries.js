be1600be70727623de0909fa9b1b311434fceaf3
Tanya <duka.tanik@gmail.com>
2022-04-11 16:06:49 +0300

    hw-10

---
+++ src/fetchCountries.js
@@ -0,0 +1,12 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name';
+const params = 'fields=name,capital,population,flags,languages'
+
+export default function fetchCountries(country) {
+    return fetch(`${BASE_URL}/${country}?${params}`)
+        .then(response => {
+            if (!response.ok) {
+            throw new Error (`Error: ${response.statusText}`)
+            }
+            return response.json();
+        })
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 import './css/styles.css';
 
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import debounce from 'lodash.debounce';
+
+import fetchCountries from './fetchCountries.js'
+
 const DEBOUNCE_DELAY = 300;
+
+const textInput = document.querySelector('input#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+textInput.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
+
+function searchCountry(event) {
+    let country = textInput.value.trim();
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+    if (country) {
+        fetchCountries(country)
+            .then(countries => checkCountry(countries))
+            .catch(() => { return Notify.failure('Oops, there is no country with that name') });
+    }
+}
+
+function checkCountry(countries) {
+    if (countries.length > 10) {
+        Notify.info("Too many matches found. Please enter a more specific name.");
+    }else{renderCountry(countries);}
+    
+}
+
+function renderCountry(countries) {
+    
+        countryList.innerHTML = countries.map(country => {
+            return `<p><img src="${country.flags.svg}" alt="flag" width="20px"> ${country.name.official}</p>`
+        }).join('');
+    
+    
+    if (countries.length === 1) {
+        const languages = Object.values(countries[0].languages).[0];
+        const markupInfo = `<ul>
+      <li>Capital: ${countries[0].capital[0]}</li>
+      <li>Population: ${countries[0].population}</li>
+      <li>Languages: ${languages}</li>
+      </ul>`;
+
+      countryInfo.insertAdjacentHTML('afterbegin', markupInfo);
+    }
+    
+}
